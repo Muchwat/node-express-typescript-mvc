ced8bb30858a468c68abb77b4a6eaa33b872440e
muchwat_kevin <kevinmuchwat@gmail.com>
2021-12-13 13:05:04 +0300

    update file upload

--- utils/file.upload.ts
+++ utils/file.upload.ts
@@ -2,7 +2,7 @@ import randomString from "./random.string";
 import path from 'path';
 
 const moveFilesTo = (directory: string, req: any, res: any) => {
-    if (!req.files) return res.status(400).send({ status: false, message: 'upload failed' });
+    if (!req.files) return res.send({ status: false, message: 'no file detected' });
 
     const file: any = req.files.file;
     const data: any = [];
@@ -10,7 +10,7 @@ const moveFilesTo = (directory: string, req: any, res: any) => {
     function move(file: any) {
         const baseName: string = randomString(16) + path.extname(file.name);
         try { file.mv(path.join(path.resolve('./public'), directory, baseName)); }
-        catch (e) { return res.status(500).send({ status: false, message: 'upload error' }); }
+        catch (e) { return res.send({ status: false, message: 'upload error' }); }
         data.push({ baseName, name: file.name, mimeType: file.mimetype, size: file.size });
     }
 
